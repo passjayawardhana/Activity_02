565f9b449231988aaa3169ee3656d05d71878462
Heshan <kanikaheshan7@gmail.com>
2021-10-06 21:58:03 +0530

    Modified python code

---
+++ activity.py
@@ -0,0 +1,9 @@
+temp=(input("Enter the Temperature:"))
+sing=raw_input("Input 'F' for Fahrenheit or 'C' for Celsius:")
+
+if sing == "F":
+    temp = ((temp-32.00)*5.00/9.00)
+    print "Temperature in Celsius: ",temp, "C"
+elif sing == "C":
+    temp = ((temp*9.00/5.00)+32.00)
+    print "Temperature in Fahrenheit: ", temp, "F"
